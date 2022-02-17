a2e47112f0cd731b05438beee3ca5915062edfe9
C <earthhominid@gmail.com>
2022-02-17 00:00:34 -0300

    shocase carousel styled components added

--- src/components/homepage/department-showcase/Chair.js
+++ src/components/homepage/department-showcase/Chair.js
@@ -1,18 +1,25 @@
+import {
+  ProductBackground,
+  ProductContainer,
+  ShowcaseImage,
+  ProductName,
+  ProductPrice,
+} from './Styles';
+
 const Chair = ({ chair }) => {
   return (
-    <div className="product-container">
-      <div className="product-background">
-        <img
-          className="showcase-image"
+    <ProductContainer>
+      <ProductBackground>
+        <ShowcaseImage
           key={chair.id}
           src={chair.image}
           alt={chair.name}
           data-id={chair.id}
         />
-      </div>
-      <h2 className="showcase-product-name">{chair.name}</h2>
-      <h2 className="showcase-product-price">{chair.amount}</h2>
-    </div>
+      </ProductBackground>
+      <ProductName>{chair.name}</ProductName>
+      <ProductPrice>{chair.amount}</ProductPrice>
+    </ProductContainer>
   );
 };
 
--- src/components/homepage/department-showcase/Styles.js
+++ src/components/homepage/department-showcase/Styles.js
@@ -46,3 +46,52 @@ export const ShowcaseCarousel = styled.div`
   margin-right: 3.688rem;
   max-height: 22.813rem;
 `;
+
+export const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  margin-left: 1.5rem;
+  max-width: 18.375rem;
+  max-height: 22.813rem;
+`;
+
+export const ProductBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #efefef;
+  opacity: 1;
+  height: 18.375rem;
+  width: 18.375rem;
+  border-radius: 5px;
+`;
+
+export const ShowcaseImage = styled.img`
+  width: 15rem;
+  height: 15rem;
+  padding: 1.688rem;
+`;
+
+export const ProductName = styled.h2`
+  text-align: center;
+  font-family: Muli-Bold;
+  font-size: 1rem;
+  line-height: 1.25rem;
+  color: #151515;
+  opacity: 1;
+  padding-top: 0.7rem;
+`;
+
+export const ProductPrice = styled.h2`
+  width: auto;
+  height: 1.125rem;
+  text-align: center;
+  font: normal normal normal 14px/20px Muli-Regular;
+  color: #000000;
+  opacity: 1;
+  padding-top: 1vh;
+  padding-bottom: 2.5vh;
+  display: block;
+`;
