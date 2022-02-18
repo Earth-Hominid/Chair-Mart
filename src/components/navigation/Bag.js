6db04e727d359e3a3dc4d813d4d7e14c99c44e5f
C <earthhominid@gmail.com>
2022-02-18 14:55:43 -0300

    bag icon styled component added

--- src/components/navigation/Bag.js
+++ src/components/navigation/Bag.js
@@ -1,18 +1,8 @@
 import BagIcon from '../../assets/images/icons/navbar/Bag.svg';
+import { StyledBagIcon } from './Navigation.styles';
 
 const Bag = () => {
-  return (
-    <div className="bag__div">
-      <div className="bag">
-        <img
-          src={BagIcon}
-          className="nav__icons"
-          alt="shopping cart"
-          id="bag"
-        />
-      </div>
-    </div>
-  );
+  return <StyledBagIcon src={BagIcon} alt="shopping cart" />;
 };
 
 export default Bag;
