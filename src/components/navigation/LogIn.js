eb90e4937c5a7c88147af12fce8df21e23e2d3d4
C <earthhominid@gmail.com>
2022-02-18 14:53:26 -0300

    styled component updated, log in icon

--- src/components/navigation/LogIn.js
+++ src/components/navigation/LogIn.js
@@ -2,14 +2,7 @@ import LogInIcon from '../../assets/images/icons/navbar/Log-in.svg';
 import { StyledLogInIcon } from './Navigation.styles';
 
 const LogIn = () => {
-  return (
-    <StyledLogInIcon
-      src={LogInIcon}
-      alt="log-in"
-      id="log-in"
-      className="nav__icons"
-    />
-  );
+  return <StyledLogInIcon src={LogInIcon} alt="log-in" />;
 };
 
 export default LogIn;
