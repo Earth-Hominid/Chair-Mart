0c688287e546d8296b79f4cd994dda0f0980dd8d
C <earthhominid@gmail.com>
2022-02-19 14:49:50 -0300

    styled components added to cart page items

--- src/components/cart-page/OrderSummary/items/Item.js
+++ src/components/cart-page/OrderSummary/items/Item.js
@@ -1,28 +1,37 @@
 import QuantityAdjuster from '../quantity-adjuster/QuantityAdjuster';
 import DeleteButton from '../delete-button/DeleteButton';
+import {
+  CartProductContainer,
+  CartProductBackground,
+  CartProductImage,
+  TextContainer,
+  ProductStyledTitle,
+  ProductStyledColor,
+  ProductStyledPrice,
+  AdjusterContainer,
+} from './Styles';
 
 const Item = ({ product, onClick }) => {
   return (
-    <div className="cart__product__container">
-      <div className="cart__product__background">
-        <img
-          className="cart__product__image"
+    <CartProductContainer>
+      <CartProductBackground>
+        <CartProductImage
           key={product.id}
           src={product.image}
           alt={product.name}
           data-id={product.id}
         />
-      </div>
-      <div className="align-center">
-        <h2 className="showcase-product-name">{product.name}</h2>
-        <h2 className="showcase-product-price">{product.amount}</h2>
-        <h2 className="showcase-product-color">Color: {product.color}</h2>
-      </div>
-      <div className="adjuster__container">
+      </CartProductBackground>
+      <TextContainer>
+        <ProductStyledTitle>{product.name}</ProductStyledTitle>
+        <ProductStyledPrice>{product.amount}</ProductStyledPrice>
+        <ProductStyledPrice>Color: {product.color}</ProductStyledPrice>
+      </TextContainer>
+      <AdjusterContainer>
         <QuantityAdjuster product={product} />
         <DeleteButton onClick={onClick} />
-      </div>
-    </div>
+      </AdjusterContainer>
+    </CartProductContainer>
   );
 };
 
---
+++ src/components/cart-page/OrderSummary/items/Styles.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+export const CartProductContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+`;
+
+export const CartProductBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #efefef;
+  border-radius: 5px;
+  height: 5rem;
+  width: 5rem;
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 1px 4px 6px rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+  }
+`;
+
+export const CartProductImage = styled.img`
+  width: auto;
+  height: 4rem;
+  padding: 0.3rem;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-around;
+`;
+
+export const ProductStyledTitle = styled.h2`
+  text-align: center;
+  font-family: Muli-Bold;
+  font-size: 1rem;
+  line-height: 1.25rem;
+  letter-spacing: 0px;
+  color: #151515;
+  opacity: 1;
+  padding-top: 0.7rem;
+`;
+
+export const ProductStyledPrice = styled.h2`
+  width: auto;
+  height: 18px;
+  text-align: center;
+  font-family: Muli-Regular;
+  font-size: 0.875rem;
+  letter-spacing: 0px;
+  color: #000000;
+  opacity: 1;
+  padding-top: 1vh;
+  padding-bottom: 2vh;
+  display: block;
+`;
+
+export const ProductStyledColor = styled.h2`
+  width: auto;
+  height: 18px;
+  text-align: center;
+  font-family: Muli-Regular;
+  font-size: 0.875rem;
+  letter-spacing: 0px;
+  color: #121212bf;
+  opacity: 1;
+  padding-top: 1vh;
+  padding-bottom: 2rem;
+  display: block;
+`;
+
+export const AdjusterContainer = styled.div`
+  display: flex;
+`;
