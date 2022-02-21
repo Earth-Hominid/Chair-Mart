1ee4239eaed4b780d89d71f8dbe72e394e56861e
C <earthhominid@gmail.com>
2022-02-21 13:51:33 -0300

    airlie images added

--- src/components/product-pages/Airlie/airlie-section/AirlieImageColumn.js
+++ src/components/product-pages/Airlie/airlie-section/AirlieImageColumn.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import chairProducts from '../../../../utils/chairProducts';
+
+import {
+  MainImageColumn,
+  MainImageContainer,
+  MainProductImage,
+  SecondaryImageGroup,
+  SecondaryImageColumn,
+} from '../../Styles';
 
 const AirlieImageColumn = () => {
-  return <div>AirlieImageColumn</div>;
+  return (
+    <>
+      <MainImageColumn>
+        <MainImageContainer>
+          <MainProductImage src={chairProducts[0].image} alt="Airlie Sofa" />
+        </MainImageContainer>
+        <SecondaryImageGroup>
+          <SecondaryImageColumn>
+            <img
+              src={chairProducts[0].imageTwo}
+              alt="Airle chair"
+              width="100%"
+            />
+          </SecondaryImageColumn>
+          <SecondaryImageColumn>
+            <img
+              src={chairProducts[0].imageThree}
+              alt=" Airliechair"
+              width="100%"
+            />
+          </SecondaryImageColumn>
+        </SecondaryImageGroup>
+        <SecondaryImageGroup>
+          <SecondaryImageColumn>
+            <img
+              src={chairProducts[0].imageFour}
+              alt=" Airliechair"
+              width="100%"
+            />
+          </SecondaryImageColumn>
+          <SecondaryImageColumn>
+            <img
+              src={chairProducts[0].imageFive}
+              alt=" Airliechair"
+              width="100%"
+            />
+          </SecondaryImageColumn>
+        </SecondaryImageGroup>
+      </MainImageColumn>
+    </>
+  );
 };
 
 export default AirlieImageColumn;
--- src/components/product-pages/Styles.js
+++ src/components/product-pages/Styles.js
@@ -11,3 +11,35 @@ export const ProductPageRow = styled.div`
   display: flex;
   justify-content: space-evenly;
 `;
+
+export const MainImageColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 50vw;
+  min-height: 60vh;
+  margin-left: 3.688rem;
+`;
+
+export const MainImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+  background-color: #efefef;
+`;
+
+export const MainProductImage = styled.img`
+  height: 32rem;
+  width: auto;
+`;
+
+export const SecondaryImageGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+`;
+
+export const SecondaryImageColumn = styled.div`
+  flex-basis: 49%;
+  background-color: #efefef;
+`;
